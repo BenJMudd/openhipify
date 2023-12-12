c68843c1007e61ebe211f4505ac22995a35118a8
benjmudd <benjmudd123@gmail.com>
2023-12-12 18:31:13 +0000

    Implementing source file argument parsing. SortSourceFilePaths will separate kernel files and source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,60 @@
-int main(int argc, char const **argv) { return 0; }
+#include "args/Arguments.h"
+#include "reporting/Defs.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "llvm/Support/Debug.h"
+#include <sstream>
+namespace ct = clang::tooling;
+
+bool SortSourceFilePaths(const std::vector<std::string> &srcList,
+                         std::vector<std::string> &kernelSrcList,
+                         std::vector<std::string> &hostSrcList) {
+  auto errMsg = [&](const std::string &file) -> std::string {
+    std::stringstream error;
+    error << sOpenHipify << sErr << "input file \"" << file
+          << "\" is a non-compatable file type."
+          << "\n";
+    return error.str();
+  };
+  for (const std::string &file : srcList) {
+    size_t dotIdx = file.rfind('.');
+    if (dotIdx == std::string::npos) {
+      llvm::errs() << errMsg(file);
+      return false;
+    }
+    std::string fileType = file.substr(dotIdx);
+    if (fileType == ".cl") {
+      kernelSrcList.push_back(file);
+    } else if (fileType == ".cpp") {
+      hostSrcList.push_back(file);
+    } else {
+      llvm::errs() << errMsg(file);
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main(int argc, const char **argv) {
+  auto cop = ct::CommonOptionsParser::create(
+      argc, argv, OpenHipifyToolTemplateCategory, llvm::cl::ZeroOrMore);
+  if (!cop) {
+    llvm::errs() << "\n" << sOpenHipify << sErr << cop.takeError() << "\n";
+    return 1;
+  }
+
+  ct::CommonOptionsParser &optParser = cop.get();
+  std::vector<std::string> srcFiles = optParser.getSourcePathList();
+  if (srcFiles.empty()) {
+    llvm::errs() << sOpenHipify << sErr << "Must specify at least 1 source file"
+                 << "\n";
+    return 1;
+  }
+
+  std::vector<std::string> kernelFiles;
+  std::vector<std::string> hostFiles;
+  if (!SortSourceFilePaths(srcFiles, kernelFiles, hostFiles))
+    return 1;
+
+  return 0;
+} // namespace clang::toolingintmain(intargc,charconst**argv)
