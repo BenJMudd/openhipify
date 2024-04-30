914acb5d8274fb330568c7cb353ec98de6dd1779
benmudd <benjmudd123@gmail.com>
2024-04-30 19:32:59 +0100

    added --no-kernel-arg-protection option

--- src/args/Arguments.cpp
+++ src/args/Arguments.cpp
@@ -1,3 +1,9 @@
 #include "args/Arguments.h"
 cl::OptionCategory
     OpenHipifyToolTemplateCategory("OpenCL to HIP transpiler options");
+
+cl::opt<bool> NoKernelArgProtection(
+    "no-kernel-arg-protection",
+    cl::desc("Lowers kernel argument range errors into warnings"),
+    cl::value_desc("no-kernel-arg-protection"),
+    cl::cat(OpenHipifyToolTemplateCategory));
