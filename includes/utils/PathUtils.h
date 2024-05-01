cba13a82ac72460118f2bea55651eb13f3a8da3f
benmudd <benjmudd123@gmail.com>
2024-05-01 10:13:26 +0100

    Removed small PathUtils file

--- includes/utils/PathUtils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "llvm/ADT/SmallVector.h"
-#include <string.h>
-namespace OpenHipify::Path {
-bool GenerateTempDuplicateFile(const std::string &file, const std::string &ext,
-                               llvm::SmallVectorImpl<char> &tmpFile);
-
-}
